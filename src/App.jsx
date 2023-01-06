2c717f758017f4dc4fed668f42489533e671a55f
OmitNomis <simon98190@gmail.com>
2023-01-06 22:45:45 +0545

    blur in

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { motion } from "framer-motion";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <h1 className="text-3xl text-black">Hello</h1>
+    <div className="h-full flex flex-col items-center p-20">
+      <h1 className="text-5xl font-bold">Hello World</h1>
+      <div className="mt-10">
+        Hi, I am going to try learning framer motion :D
+      </div>
+      <div className="mt-10">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          className="h-10 w-10 rounded-full bg-black"
+        />
+      </div>
     </div>
   );
 }
