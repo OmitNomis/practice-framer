9405cea502f193c0531fcaec615e381cca012364
OmitNomis <simon98190@gmail.com>
2023-01-08 23:30:37 +0545

    add fency radio

---
+++ src/components/FencyRadio.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+function FencyRadio({ text, onClick, selected }) {
+  return (
+    <motion.div whileTap={{ scale: 0.9 }} whileHover={{ scale: 1.1 }}>
+      <button
+        onClick={onClick}
+        className={`${
+          selected ? "border-[#852100] border-[3px]" : ""
+        } px-10 py-3 rounded-2xl  font-bold bg-neutral-700 `}
+      >
+        {text}
+      </button>
+    </motion.div>
+  );
+}
+
+export default FencyRadio;
--- src/pages/ModalStuff.jsx
+++ src/pages/ModalStuff.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import FencyButton from "../components/FencyButton";
 import FencyModal from "../components/FencyModal";
+import FencyRadio from "../components/FencyRadio";
 
 function ModalStuff() {
   const [modalVisible, setModalVisible] = useState(false);
+  const [animationStyle, setanimationStyle] = useState("fade");
+  let animateStyles = ["fade", "slide", "scale", "rotate"];
 
   const toggleModal = () => {
     setModalVisible(!modalVisible);
@@ -12,12 +15,33 @@ function ModalStuff() {
   return (
     <div className="h-full flex flex-col items-center p-20 pb-40">
       <h1 className="text-5xl font-bold">Modal Stuff</h1>
+      <div>
+        <br />
+        <p>Select Style</p>
+        <br />
+        <div className="flex">
+          {animateStyles.map((style) => (
+            <div className={` mx-3 rounded-lg`}>
+              <FencyRadio
+                onClick={() => setanimationStyle(style)}
+                key={style}
+                text={style}
+                selected={animationStyle === style ? true : false}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
       <div className="mt-10">
         <FencyButton text={"Open Modal"} onClick={toggleModal} />
       </div>
 
       <div>
-        <FencyModal isVisible={modalVisible} onClose={toggleModal} />
+        <FencyModal
+          isVisible={modalVisible}
+          onClose={toggleModal}
+          animationStyle={animationStyle}
+        />
       </div>
     </div>
   );
