e84d6b331fa209afc9c0ab88ec2abc245ced290a
OmitNomis <simon98190@gmail.com>
2023-01-11 21:13:55 +0545

    Add stagger screen component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NormalStuff from "./pages/NormalStuff";
 import ModalStuff from "./pages/ModalStuff";
 import Home from "./pages/Home";
+import StaggerList from "./pages/StaggerList";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/normal-stuff" element={<NormalStuff />} />
         <Route path="/modal-stuff" element={<ModalStuff />} />
+        <Route path="/stagger-list" element={<StaggerList />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/StaggerList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function StaggerList() {
+  return <div>StaggerList</div>;
+}
+
+export default StaggerList;
