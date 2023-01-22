accf9f74eb359396b4e61ec79409829ee90abe2d
OmitNomis <mac@OmitNomis.local>
2023-01-22 19:24:12 +0545

    add dummy data

--- src/pages/ExpandingList.jsx
+++ src/pages/ExpandingList.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
+const data = [
+  {
+    image: "https://picsum.photos/200/300",
+    title: "Card Title 1",
+    description: "Card Description 1",
+    price: "$50",
+  },
+  {
+    image: "https://picsum.photos/200/300",
+    title: "Card Title 2",
+    description: "Card Description 2",
+    price: "$100",
+  },
+  {
+    image: "https://picsum.photos/200/300",
+    title: "Card Title 3",
+    description: "Card Description 3",
+    price: "$150",
+  },
+];
+
 function ExpandingList() {
-  return <div>ExpandingList</div>;
+  return <div></div>;
 }
 
 export default ExpandingList;
