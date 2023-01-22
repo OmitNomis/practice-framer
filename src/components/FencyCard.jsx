4174fe7530e0de58152fe9509666bba422e71c52
OmitNomis <simon98190@gmail.com>
2023-01-22 20:09:17 +0545

    initialize with state and stuff

--- src/components/FencyCard.jsx
+++ src/components/FencyCard.jsx
@@ -1,36 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 
 const FencyCard = ({ data }) => {
+  const [isExpanded, setExpanded] = useState(false);
   return (
     <motion.div
-      // tailwind class for card
-      className="card flex flex-col md:flex-row items-center justify-center bg-white rounded-lg shadow-lg p-4 md:p-8"
-      initial={{ opacity: 0, x: -50 }}
-      animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0, x: 50 }}
-      whileHover={{ scale: 1.05 }}
+      className={`relative bg-gray-900 ${isExpanded ? "backdrop-blur" : ""}`}
+      animate={{
+        zIndex: isExpanded ? 100 : 0,
+        position: isExpanded ? "fixed" : "relative",
+        width: isExpanded ? "100vw" : "initial",
+        height: isExpanded ? "100vh" : "initial",
+      }}
+      initial={{
+        position: "realtive",
+        width: "initial",
+        height: "initial",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
     >
-      {/* card image, left */}
-      <motion.img
-        src={data.image}
-        alt={data.title}
-        className="w-32 h-32 md:w-48 md:h-auto md:rounded-lg"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 0.2 }}
-      />
-      {/* card content, right */}
-      <div className="flex flex-col justify-center items-start p-4 md:p-8">
-        <h2 className="text-2xl font-bold text-gray-800">{data.title}</h2>
-        <p className="text-gray-600">{data.description}</p>
-        <div className="flex items-center mt-4">
-          <h3 className="text-xl font-bold text-gray-700 mr-2">{data.price}</h3>
-          <button className="px-4 py-2 bg-gray-800 text-white rounded-lg">
-            Buy Now
-          </button>
+      <motion.div
+        className={`card flex flex-col  items-center justify-center bg-white rounded-lg shadow-lg p-4 md:p-8 max-w-[400px] `}
+        initial={{ opacity: 0, x: -50 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: 50 }}
+        whileHover={{ scale: 1.05 }}
+      >
+        <div className="">
+          <img
+            src={data.image}
+            alt={data.title}
+            className="fit-contain w-full h-full"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.2 }}
+          />
         </div>
-      </div>
+        <div className="flex  flex-col justify-center items-start p-4 md:p-8">
+          <h2 className="text-2xl font-bold text-gray-800">{data.title}</h2>
+          <p className="text-gray-600">{data.description}</p>
+          <div className="flex items-center mt-4">
+            <h3 className="text-xl font-bold text-gray-700 mr-2">
+              {data.price}
+            </h3>
+            <button
+              onClick={() => setExpanded(!isExpanded)}
+              className="px-4 py-2 bg-gray-800 text-white rounded-lg"
+            >
+              Expand
+            </button>
+          </div>
+        </div>
+      </motion.div>
     </motion.div>
   );
 };
